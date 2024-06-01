353673bee4cebf398d539df7a70a8173b7679f59
Hernando Abella <hernandoabella@gmail.com>
2024-06-01 17:53:27 -0500

    Update page.tsx

--- code-lines/app/page.tsx
+++ code-lines/app/page.tsx
@@ -121,7 +121,7 @@ const Home = () => {
                 onChange={(e) => setInputCode(e.target.value)}
                 placeholder="Insert your code here..."
               ></textarea>
-              <div className="flex">
+              <div className="flex flex-wrap">
                 <button
                   className="flex items-center bg-slate-800 text-white px-4 py-2 rounded-md mb-4 mr-2"
                   onClick={processCode}
